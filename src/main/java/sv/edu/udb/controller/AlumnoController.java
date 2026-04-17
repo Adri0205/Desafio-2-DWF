c989b77add4c72c572b070e706be91e28553f72f
Adri0205 <adrianaflores20501@gmail.com>
2026-04-17 17:00:50 -0600

    agregar: AlumnoController y MateriaController

--- src/main/java/sv/edu/udb/controller/AlumnoController.java
+++ src/main/java/sv/edu/udb/controller/AlumnoController.java
@@ -1,4 +1,36 @@
 package sv.edu.udb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sv.edu.udb.model.Alumno;
+import sv.edu.udb.service.AlumnoService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/alumnos")
 public class AlumnoController {
+
+    @Autowired
+    private AlumnoService service;
+
+    @GetMapping
+    public List<Alumno> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public Alumno guardar(@RequestBody Alumno alumno) {
+        return service.guardar(alumno);
+    }
+
+    @PutMapping("/{id}")
+    public Alumno actualizar(@PathVariable Long id, @RequestBody Alumno alumno) {
+        alumno.setId(id);
+        return service.guardar(alumno);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        service.eliminar(id);
+    }
 }
--- src/main/java/sv/edu/udb/controller/MateriaController.java
+++ src/main/java/sv/edu/udb/controller/MateriaController.java
@@ -1,4 +1,42 @@
 package sv.edu.udb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sv.edu.udb.model.Materia;
+import sv.edu.udb.service.MateriaService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/materias")
 public class MateriaController {
+
+    @Autowired
+    private MateriaService service;
+
+    @GetMapping
+    public List<Materia> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Materia obtenerPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Materia guardar(@RequestBody Materia materia) {
+        return service.guardar(materia);
+    }
+
+    @PutMapping("/{id}")
+    public Materia actualizar(@PathVariable Long id, @RequestBody Materia materia) {
+        materia.setId(id);
+        return service.guardar(materia);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        service.eliminar(id);
+    }
 }
