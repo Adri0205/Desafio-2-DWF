7aeb1e9ffb2b2d8036a0c34d067e40565b62ba1f
Adri0205 <adrianaflores20501@gmail.com>
2026-04-17 17:07:54 -0600

    agregar: ProfesorController

--- src/main/java/sv/edu/udb/controller/ProfesorController.java
+++ src/main/java/sv/edu/udb/controller/ProfesorController.java
@@ -1,4 +1,41 @@
 package sv.edu.udb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sv.edu.udb.model.Profesor;
+import sv.edu.udb.service.ProfesorService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/profesores")
 public class ProfesorController {
+
+    @Autowired
+    private ProfesorService service;
+
+    @GetMapping
+    public List<Profesor> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Profesor obtenerPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Profesor guardar(@RequestBody Profesor profesor) {
+        return service.guardar(profesor);
+    }
+
+    @PutMapping("/{id}")
+    public Profesor actualizar(@PathVariable Long id, @RequestBody Profesor profesor) {
+        profesor.setId(id);
+        return service.guardar(profesor);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        service.eliminar(id);
+    }
 }
